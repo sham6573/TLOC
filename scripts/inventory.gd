867b59e825143cd8a5d08c1a266ef26c2e2ddea6
actionkid95 <dcbarreiro31@gmail.com>
2025-04-16 11:27:44 -0600

    goblin

---
+++ scripts/inventory.gd
@@ -0,0 +1,7 @@
+extends Resource
+
+class_name Inventory
+ 
+@export var items: Array[InventoryItem]
+
+# class utlizied to store instances of InventoryItem (aka the items) in an array
---
+++ scripts/inventory_item.gd
@@ -0,0 +1,8 @@
+extends Resource
+
+class_name InventoryItem
+
+@export var name: String = ""
+@export var texture: Texture2D
+
+# class to be used as an instance of an item 
